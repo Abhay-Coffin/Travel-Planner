a828c0afd49e88dff9c77072f29da95c8b799eb7
Sharm Ayush <sharm@example.com>
2026-05-12 16:41:46 +0530

    Fix Testimonials import

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -12,7 +12,7 @@ import Newsletter from "../Shared/Newsletter";
 import ServiceList from "../Services/ServiceList";
 import FeaturedToursList from "../Components/FeaturedTours/FeaturedToursList";
 import MasonryImagesGallery from "../Components/Image-gallery/MasonryImagesGallery";
-import testimonials from "../Components/Testimonials/testimonials";
+import Testimonials from "../Components/Testimonials/testimonials";
 import FeaturedBlogsList from "../Components/FeaturedBlogs/FeaturedBlogsList";
 import NebulaBackground from "../Components/AnimatedBackground/NebulaBackground";
 
