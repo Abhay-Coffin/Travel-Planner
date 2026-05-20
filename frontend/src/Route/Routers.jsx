de8c8c0997d37961dca3515d893a28f82cbcf28c
Sharm Ayush <sharm@example.com>
2026-05-20 16:30:40 +0530

    Optimize frontend bundle with route based lazy loading

--- frontend/src/Route/Routers.jsx
+++ frontend/src/Route/Routers.jsx
@@ -1,39 +1,36 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence, motion } from "framer-motion";
 
-import Home from "../pages/Home";
-import Tours from "../pages/Tours";
-import TourDetails from "../pages/TourDetails";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
-import SearchResultList from "../pages/SearchResultList";
-import ThankYou from "../pages/ThankYou";
-import About from "../pages/About";
-import FAQ from "../Shared/FAQ";
-import Contact from "../pages/Contact";
-import Gallery from "../pages/Gallery";
-import PageNotFound from "../pages/PageNotFound";
-import Blogs from "../pages/Blogs";
-import BlogDetails from "../pages/BlogDetails";
-import Profile from "../pages/Profile";
-import Wishlist from "../pages/Wishlist";
-
 import ScrollToTop from "../utils/scrollToTop";
-import AiPlanner from "../pages/AiPlanner";
-import SavedItineraries from "../pages/SavedItineraries";
-import SharedItinerary from "../pages/SharedItinerary";
-import PublicItinerary from "../pages/PublicItinerary";
 
-import AdminDashboard from "../pages/AdminDashboard";
-import AdminItineraries from "../pages/AdminItineraries";
-import AdminTours from "../pages/AdminTours";
-import AdminBlogs from "../pages/AdminBlogs";
-import AdminBookings from "../pages/AdminBookings";
-import MyBookings from "../pages/MyBookings";
-
-import ForgotPassword from "../pages/ForgotPassword";
-import ResetPassword from "../pages/ResetPassword";
+const Home = lazy(() => import("../pages/Home"));
+const Tours = lazy(() => import("../pages/Tours"));
+const TourDetails = lazy(() => import("../pages/TourDetails"));
+const Login = lazy(() => import("../pages/Login"));
+const Register = lazy(() => import("../pages/Register"));
+const SearchResultList = lazy(() => import("../pages/SearchResultList"));
+const ThankYou = lazy(() => import("../pages/ThankYou"));
+const About = lazy(() => import("../pages/About"));
+const FAQ = lazy(() => import("../Shared/FAQ"));
+const Contact = lazy(() => import("../pages/Contact"));
+const Gallery = lazy(() => import("../pages/Gallery"));
+const PageNotFound = lazy(() => import("../pages/PageNotFound"));
+const Blogs = lazy(() => import("../pages/Blogs"));
+const BlogDetails = lazy(() => import("../pages/BlogDetails"));
+const Profile = lazy(() => import("../pages/Profile"));
+const Wishlist = lazy(() => import("../pages/Wishlist"));
+const AiPlanner = lazy(() => import("../pages/AiPlanner"));
+const SavedItineraries = lazy(() => import("../pages/SavedItineraries"));
+const PublicItinerary = lazy(() => import("../pages/PublicItinerary"));
+const AdminDashboard = lazy(() => import("../pages/AdminDashboard"));
+const AdminTours = lazy(() => import("../pages/AdminTours"));
+const AdminBlogs = lazy(() => import("../pages/AdminBlogs"));
+const AdminBookings = lazy(() => import("../pages/AdminBookings"));
+const AdminItineraries = lazy(() => import("../pages/AdminItineraries"));
+const MyBookings = lazy(() => import("../pages/MyBookings"));
+const ForgotPassword = lazy(() => import("../pages/ForgotPassword"));
+const ResetPassword = lazy(() => import("../pages/ResetPassword"));
 
 const pageVariants = {
   initial: {
@@ -71,246 +68,254 @@ const Routers = () => {
     <>
       <ScrollToTop />
 
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route
-            path="/"
-            element={
-              <PageWrapper>
-                <Home />
-              </PageWrapper>
-            }
-          />
+      <Suspense fallback={<div className="page__loader">Loading page...</div>}>
+        <AnimatePresence mode="wait">
+          <Routes location={location} key={location.pathname}>
+            <Route
+              path="/"
+              element={
+                <PageWrapper>
+                  <Home />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/about"
-            element={
-              <PageWrapper>
-                <About />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/about"
+              element={
+                <PageWrapper>
+                  <About />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/tours"
-            element={
-              <PageWrapper>
-                <Tours />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/tours"
+              element={
+                <PageWrapper>
+                  <Tours />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/tours/:id"
-            element={
-              <PageWrapper>
-                <TourDetails />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/tours/:id"
+              element={
+                <PageWrapper>
+                  <TourDetails />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/login"
-            element={
-              <PageWrapper>
-                <Login />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/login"
+              element={
+                <PageWrapper>
+                  <Login />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/register"
-            element={
-              <PageWrapper>
-                <Register />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/register"
+              element={
+                <PageWrapper>
+                  <Register />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/thank-you"
-            element={
-              <PageWrapper>
-                <ThankYou />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/thank-you"
+              element={
+                <PageWrapper>
+                  <ThankYou />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/search"
-            element={
-              <PageWrapper>
-                <SearchResultList />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/search"
+              element={
+                <PageWrapper>
+                  <SearchResultList />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/faq"
-            element={
-              <PageWrapper>
-                <FAQ />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/faq"
+              element={
+                <PageWrapper>
+                  <FAQ />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/gallery"
-            element={
-              <PageWrapper>
-                <Gallery />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/gallery"
+              element={
+                <PageWrapper>
+                  <Gallery />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/contact"
-            element={
-              <PageWrapper>
-                <Contact />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/contact"
+              element={
+                <PageWrapper>
+                  <Contact />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/blogs"
-            element={
-              <PageWrapper>
-                <Blogs />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/blogs"
+              element={
+                <PageWrapper>
+                  <Blogs />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/blogs/:id"
-            element={
-              <PageWrapper>
-                <BlogDetails />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/blogs/:id"
+              element={
+                <PageWrapper>
+                  <BlogDetails />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/profile"
-            element={
-              <PageWrapper>
-                <Profile />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/profile"
+              element={
+                <PageWrapper>
+                  <Profile />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/wishlist"
-            element={
-              <PageWrapper>
-                <Wishlist />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/wishlist"
+              element={
+                <PageWrapper>
+                  <Wishlist />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/ai-planner"
-            element={
-              <PageWrapper>
-                <AiPlanner />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/ai-planner"
+              element={
+                <PageWrapper>
+                  <AiPlanner />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/saved-itineraries"
-            element={
-              <PageWrapper>
-                <SavedItineraries />
-              </PageWrapper>
-            }
-          />
-          <Route path="/shared/:shareId" element={<PageWrapper><PublicItinerary /></PageWrapper>} />
+            <Route
+              path="/saved-itineraries"
+              element={
+                <PageWrapper>
+                  <SavedItineraries />
+                </PageWrapper>
+              }
+            />
 
-          
+            <Route
+              path="/shared/:shareId"
+              element={
+                <PageWrapper>
+                  <PublicItinerary />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/admin"
-            element={
-              <PageWrapper>
-                <AdminDashboard />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/admin"
+              element={
+                <PageWrapper>
+                  <AdminDashboard />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-  path="/admin/itineraries"
-  element={
-    <PageWrapper>
-      <AdminItineraries />
-    </PageWrapper>
-  }
-/>
+            <Route
+              path="/admin/itineraries"
+              element={
+                <PageWrapper>
+                  <AdminItineraries />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/admin/tours"
-            element={
-              <PageWrapper>
-                <AdminTours />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/admin/tours"
+              element={
+                <PageWrapper>
+                  <AdminTours />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/admin/blogs"
-            element={
-              <PageWrapper>
-                <AdminBlogs />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/admin/blogs"
+              element={
+                <PageWrapper>
+                  <AdminBlogs />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/admin/bookings"
-            element={
-              <PageWrapper>
-                <AdminBookings />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/admin/bookings"
+              element={
+                <PageWrapper>
+                  <AdminBookings />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/forgot-password"
-            element={
-              <PageWrapper>
-                <ForgotPassword />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/forgot-password"
+              element={
+                <PageWrapper>
+                  <ForgotPassword />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/reset-password"
-            element={
-              <PageWrapper>
-                <ResetPassword />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/reset-password"
+              element={
+                <PageWrapper>
+                  <ResetPassword />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="/my-bookings"
-            element={
-              <PageWrapper>
-                <MyBookings />
-              </PageWrapper>
-            }
-          />
+            <Route
+              path="/my-bookings"
+              element={
+                <PageWrapper>
+                  <MyBookings />
+                </PageWrapper>
+              }
+            />
 
-          <Route
-            path="*"
-            element={
-              <PageWrapper>
-                <PageNotFound />
-              </PageWrapper>
-            }
-          />
-        </Routes>
-      </AnimatePresence>
+            <Route
+              path="*"
+              element={
+                <PageWrapper>
+                  <PageNotFound />
+                </PageWrapper>
+              }
+            />
+          </Routes>
+        </AnimatePresence>
+      </Suspense>
     </>
   );
 };
